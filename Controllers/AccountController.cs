94d217e58654fcc0485d0ae572f50ddd5d91bfdd
priyanka <priyankatripathy81@gmail.com>
2024-02-24 13:14:53 +0530

    add files

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankLoginPage1._20.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+         
+            return RedirectToAction("Index", "Home"); 
+        }
+
+        public IActionResult Logout()
+        {
+            
+            return RedirectToAction("Login");
+        }
+    }
+}
